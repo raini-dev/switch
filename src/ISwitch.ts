25355a545b5073a308776106dc059aef0f64cd8a
Sergei Orlov <sergei_orlov@epam.com>
2020-01-15 17:50:42 +0300

    feat(case): add support for returning various values
    The type of each value returned will be added to intersection of types upon forking the Switch with s.default()

--- src/ISwitch.ts
+++ src/ISwitch.ts
@@ -1,5 +1,6 @@
 import { TNonFunction, TPredicateFunction } from "./TPredicateFunction";
 import { TKnown } from "./TKnown";
+import { Unpack } from "./Switch";
 
 /**
  * ISwitch for cases when K is defined.
@@ -14,16 +15,16 @@ export interface ISwitch<T, K> {
   /**
    * Define predicate to be matched against and the value to be returned in case of matching.
    */
-  case<N>(pred: TNonFunction<T>, value: TKnown<K, N>): TDefinedSwitch<T, K, N>;
+  case<N>(pred: TNonFunction<T>, value: N): ISwitch<T, [Unpack<K>, N]>;
 
   /**
    * Define predicate function to be executed against Switch state and the value to be
    * returned in case of matching.
    */
-  case<N>(pred: TPredicateFunction<T>, value: TKnown<K, N>): TDefinedSwitch<T, K, N>;
+  case<N>(pred: TPredicateFunction<T>, value: N): ISwitch<T, [Unpack<K>, N]>;
 
   /**
    * Define the value to be returned in case none of the cases was matched.
    */
-  default<N>(defaultValue: N): K | N;
+  default<N>(defaultValue: N): Unpack<K> | N;
 }
