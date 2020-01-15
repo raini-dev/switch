a406395bfb34050c56cc05acc8445f8e419549ad
Sergei Orlov <sergei_orlov@epam.com>
2020-01-15 23:10:22 +0300

    fix(switch): improve type inference

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Switch } from "./Switch";
+import { ISwitch } from "./ISwitch";
 
 export * from "./ISwitch";
 export * from "./Switch";
@@ -8,6 +9,6 @@ export * from "./TPredicateFunction";
 /**
  * Pointer interface for lifting value provided as an argument to Switch.
  */
-export default function<T, K extends any[]>(x: T) {
+export default function<T, K extends []>(x: T): ISwitch<T, K> {
   return Switch.for<T, K>(x);
 }
